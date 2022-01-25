4fb8e10bf5b83b7439e3a4c24dd3a447d48c277f
aziz <ahmed613@umn.edu>
2022-01-24 21:35:13 -0600

    added redux store and updated reducers to reflect redux

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+//import { useReducer } from "react";
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -11,7 +11,16 @@ import {
   TOGGLE_CART
 } from "./actions";
 
-export const reducer = (state, action) => {
+const appState = {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: ''
+
+}
+
+export const reducer = (state = appState, action) => {
   switch (action.type) {
     case UPDATE_PRODUCTS:
       return {
